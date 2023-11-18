b014e1841c2e7f700a8194f4d5eb09ce96e03b63
clayton-allard <clayton.allard@stat.ubc.ca>
2023-11-18 15:34:18 -0800

    created dropdown for datasets

--- Orange/app.R
+++ Orange/app.R
@@ -10,6 +10,43 @@ conflict_prefer('filter', winner = 'dplyr')
 conflict_prefer('lag', winner = 'dplyr')
 library(shiny)
 library(tidyverse)
+library(datasets)
+library(purrr)
+
+# used to subset datasets that have at least one numeric column
+has_numeric_variable <- function(dataset){
+  # only include if it is loadable
+  tryCatch({
+      # True if at least one column is numeric
+      any(sapply(dataset, is.numeric))
+    }, 
+    error = function(e) {
+      FALSE
+    }
+  )
+}
+
+# get all numeric/character variables that have less than 10 unique values
+get_typeof_vars <- function(dataset, type='numeric'){
+  vars <-  names(dataset)
+  type_vars <-  vars[sapply(dataset, function(col) {class(col) == type})]
+  # to not overload the plot
+  if (type == 'character'){
+    type_vars <-  vars[sapply(dataset, function(col) {unique(dataset[col]) <= 10})]
+  }
+  return(type_vars)
+}
+
+# get a list of all base R data sets that have at least one numeric variable
+data_names <- data(package = 'datasets')$results[, "Item"]
+data_names <- keep(data_names, has_numeric_variable)
+
+# so that we can initialize the dataframe
+random_name <- sample(data_names, 1)
+random_dataset <- get(random_name)
+num_vars <-  get_typeof_vars(random_dataset)
+sample_num <-  sample(num_vars, 1)
+char_vars <-  get_typeof_vars(random_dataset)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -19,25 +56,32 @@ ui <- fluidPage(
     h4("Use this app to explore content on oranges"),
     sidebarLayout(
       sidebarPanel(
+        # NEW FEATURE 1: allow the user to choose dataset (from base R)
+        # select variable to plot
+        selectInput("dataset", "Select dataset:", 
+                    choices=data_names, selected = random_dataset),
+        
         # FEATURE 1: allow the user to choose variable to plot by
         # select variable to plot
         selectInput("dropdown", "Select variable:", 
-                    choices=c('age', 'circumference'), selected = 'age'),
+                    choices=num_vars,
+                    selected = sample_num),
         
         # FEATURE 2: allow user to select a range for the variable they chose.
         # select range of variable to visualize
-        sliderInput("id_slider", "Select a age range:", 
-                    min = min(Orange$age), max = max(Orange$age), 
-                    value = range(Orange$age)),
+        sliderInput("id_slider", "Select ", sample_num," range:", 
+                    min = min(random_dataset[sample_num]), 
+                    max = max(random_dataset[sample_num]), 
+                    value = range(random_dataset[sample_num])),
         
         # FEATURE 3: allow user to select the number of bins.
         # select number of bins for histogram
         sliderInput("bins", "Number of bins:", min = 1,max = 30,value = 10),
-      
-        # FEATURE 4: allow user to select whether to group by tree.
-        # choose whether to group by tree type
-        checkboxInput("by_tree", "Check box to group by tree", FALSE),
-                      ),
+        
+        # group by
+        selectInput("group_by", "Select variable to group by:", 
+                    choices=c('', char_vars),
+                    selected = ''),
       mainPanel(
         # output a histogram and a table
         plotOutput("id_histogram"),
@@ -45,51 +89,60 @@ ui <- fluidPage(
         DT::dataTableOutput("id_table")
       )
     )
+    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
-  # observe(print(input$id_slider))
+  dataset <- reactive({
+    get(input$dataset)
+  })
   
-  orange_data <- reactive({
-    Orange$Tree <- factor(Orange$Tree, levels=as.character(1:5))
-    Orange %>%
+  filtered_data <- reactive({
+    dataset() %>%
     dplyr::filter(!!sym(input$dropdown) >= input$id_slider[1],
            !!sym(input$dropdown) <= input$id_slider[2])
   })
   
   observe({
+    data = dataset()
+    
+    # Update variable options once a dataset is chosen
+    updateSelectInput(session, "dropdown", 
+                      label = paste("Select variable:"),
+                      choices = get_typeof_vars(data)
+                      )
     
     # Update the slider input after selecting dropdown
     updateSliderInput(session, "id_slider", 
-                      value = range(Orange[input$dropdown]),
+                      value = range(data[input$dropdown]),
                       label = paste("Select a ", input$dropdown, " range:"),
-                      min = min(Orange[input$dropdown]),
-                      max = max(Orange[input$dropdown]))
+                      min = min(data[input$dropdown]),
+                      max = max(data[input$dropdown]))
   })
   
   # plot histogram
   output$id_histogram <- renderPlot({
-    # plot by specific tree
-    if(input$by_tree){
-      orange_data() %>%  
-        ggplot(aes(x = !!sym(input$dropdown), fill = Tree)) +
-          geom_histogram(bins=input$bins, color='black', alpha=0.7)+
-          theme_classic(20)
-    } else {
+    if(input$group_by == ''){
       # plot all as the same
       orange_data() %>%  
         ggplot(aes(x = !!sym(input$dropdown))) +
           geom_histogram(bins=input$bins, fill='orange', color='black', 
                          alpha=0.7)+
           theme_classic(20)
+    } else {
+      # plot by group
+      orange_data() %>%  
+        ggplot(aes(x = !!sym(input$dropdown), fill = input$group_by)) +
+          geom_histogram(bins=input$bins, color='black', alpha=0.7)+
+          theme_classic(20)
     }
   })
   
   # create interactive table
   output$id_table <- DT::renderDataTable({
-    orange_data()
+    dataset()
   })
 }
 
