cadb93190bb4502dc83d929a91f0a3f17c73318c
clayton-allard <clayton.allard@stat.ubc.ca>
2023-11-18 18:57:25 -0800

    Got it to run, but need to fix errors

--- Orange/app.R
+++ Orange/app.R
@@ -18,8 +18,7 @@ has_numeric_variable <- function(dataset){
   # only include if it is loadable
   tryCatch({
       data = get(dataset)
-      # True if at least one column is numeric
-      any(sapply(data.frame(data), is.numeric))
+      return(any(sapply(data.frame(data), is.numeric)))
     },
     error = function(e) {
       return(FALSE)
@@ -56,19 +55,23 @@ random_dataset <- data.frame(get(random_name))
 num_vars <-  get_typeof_vars(random_dataset)
 sample_num <-  sample(num_vars, 1)
 char_vars <- get_typeof_vars(random_dataset, 'character')
+numeric_col <- as.numeric(random_dataset[[sample_num]])
+
+print(random_name)
+print(sample_num)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Orange Data App"),
-    h4("Use this app to explore content on oranges"),
+    titlePanel("R Dataset App"),
+    h4("Use this app to explore content on all base R datasets"),
     sidebarLayout(
       sidebarPanel(
         # NEW FEATURE 1: allow the user to choose dataset (from base R)
         # select variable to plot
-        selectInput("dataset", "Select dataset:", 
-                    choices=data_names, selected = random_dataset),
+        selectInput("dataset", "Select dataset:",
+                    choices=data_names, selected = random_name),
         
         # FEATURE 1: allow the user to choose variable to plot by
         # select variable to plot
@@ -78,10 +81,10 @@ ui <- fluidPage(
         
         # FEATURE 2: allow user to select a range for the variable they chose.
         # select range of variable to visualize
-        sliderInput("id_slider", "Select ", sample_num," range:", 
-                    min = min(random_dataset[sample_num]), 
-                    max = max(random_dataset[sample_num]), 
-                    value = range(random_dataset[sample_num])),
+        sliderInput("id_slider", paste("Select ", sample_num, "range:"),
+                    min = min(numeric_col),
+                    max = max(numeric_col),
+                    value = range(numeric_col)),
         
         # FEATURE 3: allow user to select the number of bins.
         # select number of bins for histogram
@@ -90,22 +93,22 @@ ui <- fluidPage(
         # group by
         selectInput("group_by", "Select variable to group by:", 
                     choices=c('', char_vars),
-                    selected = ''),
+                    selected = '')),
       mainPanel(
         # output a histogram and a table
         plotOutput("id_histogram"),
         # FEATURE 5: add an interactive table sorting by any variable.
         DT::dataTableOutput("id_table")
       )
-    )
+    
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  
-  dataset <- reactive({
-    get(input$dataset)
+
+    dataset <- reactive({
+      data.frame(get(input$dataset))
   })
   
   filtered_data <- reactive({
@@ -115,37 +118,47 @@ server <- function(input, output, session) {
   })
   
   observe({
-    data = dataset()
+    data <-  dataset()
+    
+    numeric_col <- as.numeric(data[[input$dropdown]])
+    num_vars <- get_typeof_vars(data)
     
     # Update variable options once a dataset is chosen
-    updateSelectInput(session, "dropdown", 
+    updateSelectInput(session, "dropdown",
                       label = paste("Select variable:"),
-                      choices = get_typeof_vars(data)
+                      choices = num_vars,
+                      selected = sample(num_vars, 1)
                       )
-    
+
     # Update the slider input after selecting dropdown
-    updateSliderInput(session, "id_slider", 
-                      value = range(data[input$dropdown]),
+    updateSliderInput(session, "id_slider",
+                      value = range(numeric_col),
                       label = paste("Select a ", input$dropdown, " range:"),
-                      min = min(data[input$dropdown]),
-                      max = max(data[input$dropdown]))
+                      min = min(numeric_col),
+                      max = max(numeric_col))
+    
+    # Update group by options
+    updateSelectInput(session, "group_by",
+                      label = paste("Select variable:"),
+                      choices = c('', get_typeof_vars(data, 'character')))
   })
   
   # plot histogram
   output$id_histogram <- renderPlot({
     if(input$group_by == ''){
       # plot all as the same
-      orange_data() %>%  
+      filtered_data() %>%  
         ggplot(aes(x = !!sym(input$dropdown))) +
           geom_histogram(bins=input$bins, fill='orange', color='black', 
                          alpha=0.7)+
           theme_classic(20)
     } else {
       # plot by group
-      orange_data() %>%  
+      filtered_data() %>%  
         ggplot(aes(x = !!sym(input$dropdown), fill = input$group_by)) +
           geom_histogram(bins=input$bins, color='black', alpha=0.7)+
-          theme_classic(20)
+          theme_classic(20) +
+          labs(fill = input$group_by)
     }
   })
   
